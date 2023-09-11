adf2ddac45dbaad871e84137cb301686103a43c9
Christian Paisley <cepaisley13@gmail.com>
2023-09-10 17:55:35 -0700

    Change About, Mission, and Vision

--- src/Pages/Primary/Home/Tabbed/tabbedData.js
+++ src/Pages/Primary/Home/Tabbed/tabbedData.js
@@ -1,24 +1,24 @@
-import image1 from './img/Alex-Esqueda-Box-Jump.jpg';
-import image2 from './img/Tire-Flip.jpg';
-import image3 from './img/Kids.jpg';
+import image1 from "./img/Alex-Esqueda-Box-Jump.jpg";
+import image2 from "./img/Tire-Flip.jpg";
+import image3 from "./img/Kids.jpg";
 
 export const tabbedData = [
     {
         id: 1,
         image: image1,
-        title: 'About',
-        text: '<p>Join a <b>community</b> of people working hard every day to live a better quality of life, and have fun doing it!  At CrossFit Decimate, we provide inclusive programming, individualized modifications, and top-trained coaches to give you the best fitness journey  to fit your lifestyle.</p>',
+        title: "About",
+        text: "<p>At <b>Curls</b>, we are more than just a hair care company; we are a testament to resilience, innovation, and the power of community. Founded during the challenging times of the COVID-19 pandemic in the vibrant heart of Los Angeles, California, our journey embodies the spirit of determination and the desire to bring a positive change to the world of beauty. As a Black-owned business, we take immense pride in our roots, drawing inspiration from the rich tapestry of our culture and heritage. Our mission is to provide inclusive, high-quality hair care solutions that cater to the unique needs of diverse hair types. We understand the struggles and the beauty of every curl, kink, and coil, and we're committed to empowering individuals to embrace their natural hair with confidence.</p>",
     },
     {
         id: 2,
         image: image2,
-        title: 'Mission',
-        text: '<p>At Decimate, we believe that just like your fitness journey, nutrition is not a “one size fits all”.  Our highly trained nutrition coaches work alongside you to help you achieve your optimal health and fitness goals.  Through one-on-one coaching and personalized nutrition programming, our coaches are able to determine the best nutrition plan for you.</p>',
+        title: "Mission",
+        text: "<p>Our unwavering dedication to quality and authenticity drives us to create products that cater to diverse hair types, nurturing healthy, vibrant locks while fostering self-love and self-expression. We aim to redefine industry standards by challenging conventions, promoting inclusivity, and championing the natural beauty that resides within each strand. We believe that every person deserves to feel confident, radiant, and proud of their hair, and we're here to make that vision a reality. With <b>Curls</b>, it's not just about hair care; it's about celebrating the essence of who you are. Join us as we continue to lead the way in the world of hair care, one beautiful head of hair at a time.</p>",
     },
     {
         id: 3,
         image: image3,
-        title: 'Vision',
-        text: '<p>With our highly qualified and trained coach, Kara Smith, CrossFit Decimate is able to provide adaptive and inclusive workouts for each individual athlete, whether you are injured or working with a disability.  At CrossFit Decimate we believe that all people can workout and want to workout, they just need to be guided on how to achieve their goals.  Coach Kara offers modifications to each workout to help each athlete achieve their own level of success.  </p>',
+        title: "Vision",
+        text: "<p>In our vision, every person, regardless of their hair type or texture, will have access to high-quality, tailored hair care solutions that enhance and nourish their natural locks. We envision a future where everyone feels empowered to express their unique style and confidence through their hair, free from societal pressures and stigmas. As we continue to grow and evolve, we are committed to fostering a diverse and supportive community, where knowledge is shared, connections are formed, and self-love is celebrated. Through our products, initiatives, and collaborations, we aim to be a catalyst for positive change in the beauty industry and a source of inspiration for individuals around the world. Our journey is guided by the belief that every curl, coil, and kink tells a unique story, and we are dedicated to helping you tell yours with pride. Join us on this transformative path towards a world where natural hair is not only accepted but adored—a world where your hair reflects your true self.",
     },
 ];

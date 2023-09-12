39670b7267b69e17c0a36fd9b8a6feb7fec2550f
Christian Paisley <cepaisley13@gmail.com>
2023-09-11 17:50:27 -0700

    Resize SiteLogo

--- src/Common/PagesLayout/SiteLogo/SiteLogo.jsx
+++ src/Common/PagesLayout/SiteLogo/SiteLogo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 /* Images ---------------------------*/
-import logoUrl from "./Logo + slogan resize.png";
+import logoUrl from "./Updated Lgo.png";
 
 const SiteLogo = () => {
     return (
@@ -20,7 +20,7 @@ const SiteLogoStyled = styled.div`
     height: 100%;
 
     img {
-        width: 300px;
-        height: 300px;
+        width: 200px;
+        height: 200px;
     }
 `;
